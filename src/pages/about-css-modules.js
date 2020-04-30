0bf41a168615bad061ed6102d57eaa09d4499aa1
riya-kondo <hosa.ashes@gmail.com>
2020-04-30 15:33:33 +0900

    create style files based on tutorial#2

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Container from "../components/container"
+import Header from "../components/header"
+
+export default () => (
+  <Container>
+    <Header headerText="About CSS Modules" />
+    <p>CSS Modules are cool</p>
+  </Container>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,6 @@ export default () => (
     <Link to="/contact/">Contact</Link>
     <Header headerText="Hello Gatsby"/>
     <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="random image"/>
+    <img src="https://source.unsplash.com/random/400x200" alt="randomized"/>
   </div>
 )
