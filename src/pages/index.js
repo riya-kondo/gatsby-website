61e606641ee3fe5358aff72ad9fe41edd0f7447c
riya-kondo <hosa.ashes@gmail.com>
2020-04-30 16:20:19 +0900

    typography plugin is installed to change style based on tutorial#3

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import Header from "../components/Header"
 
 export default () => (
-  <div style={{color: 'purple'}}>
+  <div style={{ margin: "3rem auto", maxWidth: 600 }}>
     <Link to="/contact/">Contact</Link>
     <Header headerText="Hello Gatsby"/>
     <p>What a world.</p>
