a390ec261bc46c9a5ccc7c38ab1204f167d1de9a
riya-kondo <hosa.ashes@gmail.com>
2020-05-01 16:23:51 +0900

    Based on tutorial #6, md transformer and blog list are added.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,46 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 
-export default () => (
-  <Layout>
-    <h1>Hello Gatsby</h1>
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="randomized"/>
-  </Layout>
-)
+export default ({ data }) => {
+  return (
+    <Layout>
+      <h1>Hello Gatsby</h1>
+      <p>What a world.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="randomized"/>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title + " "}
+            <span>
+              - {node.frontmatter.date}
+            </span>
+          </h3>
+          <p>{node.excerpt}</p> 
+        </div>
+      ))}
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+          timeToRead
+          html
+          id
+        }
+      }
+      totalCount
+    }
+  }
+`
