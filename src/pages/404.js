f7c23e70fdd2467aa06025a00018753fa587826d
riya-kondo <hosa.ashes@gmail.com>
2020-05-09 13:53:26 +0900

    custom 404 page is added

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+export default () => {
+  return (
+    <Layout>
+      <h1>Page not found</h1>
+      <p>The page you are looking for has been removed or relocated.</p>
+      <img src="https://source.unsplash.com/random/400x200" alt="randomized"/>
+      <Link to="/">Go Back</Link>
+    </Layout>
+  )
+}
