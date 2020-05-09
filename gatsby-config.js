0444f8ea8b5011081d3c283e5207490335ec3e37
riya-kondo <hosa.ashes@gmail.com>
2020-05-09 13:36:41 +0900

    tutorial#8 is done. website is optimized for chrome auditss

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,23 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: 'Title from siteMetadata'
+    title: 'Title from siteMetadata',
+    description: 'This is a blog about pandas',
+    author: 'gatsbyjs',
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'GatsbyJS',
+        short_name: 'GatsbyJS',
+        start_url: '/',
+        background_color: '#6b37bf',
+        theme_color: '#6b37bf',
+        display: 'standalone',
+        icon: 'src/images/icon.png'
+      }
+    },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -17,13 +31,17 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    'gatsby-transformer-remark',
-    'gatsby-plugin-emotion',
     {
       resolve: 'gatsby-plugin-typography',
       options: {
         pathToConfigModule: 'src/utils/typography',
       },
     },
+    'gatsby-transformer-remark',
+    'gatsby-plugin-emotion',
+    // オフラインプラグインは作成されたファイルをキャッシュするように
+    // manifestプラグインの後にリストする必要がある。
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
   ],
 }
