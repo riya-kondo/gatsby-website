65ae251668d0a3d0cb4edfd25df7fea4f9f9fc1c
riya-kondo <hosa.ashes@gmail.com>
2020-05-11 22:18:44 +0900

    404 image is fixed using webpack assets importing.

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+// Webpackに画像ファイルの利用を通知する
+import berochan from "../images/404/berochan.jpg"
 
 export default () => {
   return (
     <Layout>
       <h1>Page not found</h1>
       <p>The page you are looking for has been removed or relocated.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="randomized"/>
+      <img src={berochan} alt="ayachan stick outs tongue with sleeping." />
       <div>
         <Link to="/">Go Back Top Page</Link>
       </div>
