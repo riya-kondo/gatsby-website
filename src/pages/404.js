28cb693515d9605c5683503c7fc2c8614a2fb122
riya-kondo <hosa.ashes@gmail.com>
2020-05-11 21:23:59 +0900

    netlify is installed and 404 page is fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,7 +9,9 @@ export default () => {
       <h1>Page not found</h1>
       <p>The page you are looking for has been removed or relocated.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="randomized"/>
-      <Link to="/">Go Back</Link>
+      <div>
+        <Link to="/">Go Back Top Page</Link>
+      </div>
     </Layout>
   )
 }
