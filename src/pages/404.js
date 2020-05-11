c840aea150ae1c04da306c190218be0947bd4474
riya-kondo <hosa.ashes@gmail.com>
2020-05-11 22:34:30 +0900

    Image file in 404 page is randomized from graphql query

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,28 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-// Webpackに画像ファイルの利用を通知する
-import berochan from "../images/404/berochan.jpg"
 
 export default () => {
+  const data = useStaticQuery(graphql`
+    {
+      allFile(filter: {extension: {eq: "jpg"}, dir: {regex: "/404/"}}){
+        edges {
+          node {
+            name
+            publicURL
+          }
+        }
+      }
+    }
+  `)
+  const index = Math.floor( Math.random() * data.allFile.edges.length)
+  const imageFile = data.allFile.edges[index].node
   return (
     <Layout>
       <h1>Page not found</h1>
       <p>The page you are looking for has been removed or relocated.</p>
-      <img src={berochan} alt="ayachan stick outs tongue with sleeping." />
+      <img src={imageFile.publicURL} alt={imageFile.name} />
       <div>
         <Link to="/">Go Back Top Page</Link>
       </div>
